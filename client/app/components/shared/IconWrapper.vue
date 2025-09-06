9d331efee9aefbec1f33b34701c9398cc353b8c0
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-07 00:00:13 +0200

    IconWrapper Partial

--- client/app/components/shared/IconWrapper.vue
+++ client/app/components/shared/IconWrapper.vue
@@ -8,14 +8,11 @@ const props = withDefaults(
 	{ size: "md" }
 );
 
-const sizeClass: Record<Size, string> = {
-	xxs: "w-6 h-6 p-xxs",
-	xs: "w-7 h-7 p-xxs",
+const sizeClass: Partial<Record<Size, string>> = {
 	sm: "w-8 h-8 p-xxs",
 	md: "w-10 h-10 p-xs",
 	lg: "w-12 h-12 p-xs",
 	xl: "w-16 h-16 p-xs",
-	xxl: "w-20 h-20 p-sm",
 };
 </script>
 
