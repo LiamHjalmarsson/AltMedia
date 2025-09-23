02183e22b8bf2ea7e580aa46318dee4b83287d69
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-23 22:31:48 +0200

    Articles

--- cms/src/api/article/controllers/article.ts
+++ cms/src/api/article/controllers/article.ts
@@ -1,7 +1,37 @@
 /**
  * article controller
  */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController("api::article.article", ({ strapi }) => ({
+	async find(ctx) {
+		await this.validateQuery(ctx);
 
-export default factories.createCoreController('api::article.article');
+		const sanitizedQuery = await this.sanitizeQuery(ctx);
+
+		const { results, pagination } = await strapi.service("api::article.article").getAllArticles(sanitizedQuery);
+
+		const sanitizedEntity = await this.sanitizeOutput(results, ctx);
+
+		return this.transformResponse(sanitizedEntity, { pagination });
+	},
+
+	async findOne(ctx) {
+		await this.validateQuery(ctx);
+
+		const sanitizedQuery = await this.sanitizeQuery(ctx);
+
+		const { id } = ctx.params;
+
+		const entity = await strapi.service("api::article.article").getArticleBySlug(sanitizedQuery, id);
+
+		if (!entity) {
+			return ctx.notFound("Article not found");
+		}
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+		return this.transformResponse(sanitizedEntity);
+	},
+}));
+
--- cms/src/api/article/services/article.ts
+++ cms/src/api/article/services/article.ts
@@ -2,6 +2,59 @@
  * article service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
+import {
+	fullSectionPopulateBlock,
+	imagePopulate,
+	infoBlockPopulate,
+	introductionBlockPopulate,
+	listPopulateBlock,
+} from "../../../utils/populate";
 
-export default factories.createCoreService('api::article.article');
+export default factories.createCoreService("api::article.article", ({ strapi }) => ({
+	async getAllArticles(sanitizedQuery) {
+		return strapi.service("api::article.article").find({
+			...sanitizedQuery,
+			fields: ["id", "title", "slug", "description", "published_date", "reading_time_min"],
+			populate: {
+				cover: imagePopulate,
+				services: {
+					fields: ["title", "slug"],
+				},
+			},
+		});
+	},
+
+	async getArticleBySlug(sanitizedQuery, slug: string) {
+		const { results } = await strapi.service("api::article.article").find({
+			...sanitizedQuery,
+			filters: { slug },
+			fields: ["id", "title", "slug", "description", "published_date", "reading_time_min"],
+			populate: {
+				cover: imagePopulate,
+				services: {
+					fields: ["title", "slug"],
+				},
+				hero: heroPopulate,
+				introduction: introductionBlockPopulate,
+				blocks: {
+					on: {
+						"block.list": listPopulateBlock,
+						"block.info": infoBlockPopulate,
+						"block.full-section": fullSectionPopulateBlock,
+					},
+				},
+			},
+		});
+
+		return results?.[0] ?? null;
+	},
+}));
+
+const heroPopulate = {
+	fields: ["title", "description", "colored_words", "align_content"],
+	populate: {
+		cover: imagePopulate,
+	},
+};
+
