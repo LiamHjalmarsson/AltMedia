07d7dbaecd26608cfc0492a996812bf6b3520bde
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-23 22:28:41 +0200

    Subservices

--- cms/src/api/subservice/controllers/subservice.ts
+++ cms/src/api/subservice/controllers/subservice.ts
@@ -1,7 +1,39 @@
 /**
  * subservice controller
  */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
+export default factories.createCoreController("api::subservice.subservice", ({ strapi }) => ({
+	async find(ctx) {
+		await this.validateQuery(ctx);
 
-export default factories.createCoreController('api::subservice.subservice');
+		const sanitizedQuery = await this.sanitizeQuery(ctx);
+
+		const { results, pagination } = await strapi
+			.service("api::subservice.subservice")
+			.getAllSubservices(sanitizedQuery);
+
+		const sanitizedEntity = await this.sanitizeOutput(results, ctx);
+
+		return this.transformResponse(sanitizedEntity, { pagination });
+	},
+
+	async findOne(ctx) {
+		await this.validateQuery(ctx);
+
+		const sanitizedQuery = await this.sanitizeQuery(ctx);
+
+		const { id } = ctx.params;
+
+		const entity = await strapi.service("api::subservice.subservice").getSubserviceBySlug(sanitizedQuery, id);
+
+		if (!entity) {
+			return ctx.notFound("Subservice not found");
+		}
+
+		const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+		return this.transformResponse(sanitizedEntity);
+	},
+}));
+
--- cms/src/api/subservice/services/subservice.ts
+++ cms/src/api/subservice/services/subservice.ts
@@ -2,6 +2,63 @@
  * subservice service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
+import {
+	fullSectionPopulateBlock,
+	imagePopulate,
+	infoBlockPopulate,
+	introductionBlockPopulate,
+	listPopulateBlock,
+} from "../../../utils/populate";
 
-export default factories.createCoreService('api::subservice.subservice');
+export default factories.createCoreService("api::subservice.subservice", ({ strapi }) => ({
+	async getAllSubservices(sanitizedQuery) {
+		return strapi.service("api::subservice.subservice").find({
+			...sanitizedQuery,
+			fields: ["id", "title", "slug", "content"],
+			populate: {
+				tags: {
+					fields: ["title"],
+				},
+				service: {
+					fields: ["title", "slug"],
+				},
+			},
+		});
+	},
+
+	async getSubserviceBySlug(sanitizedQuery, slug: string) {
+		const { results } = await strapi.service("api::subservice.subservice").find({
+			...sanitizedQuery,
+			filters: { slug },
+			fields: ["id", "title", "slug", "content"],
+			populate: {
+				tags: {
+					fields: ["title"],
+				},
+				service: {
+					fields: ["title", "slug"],
+				},
+				hero: heroPopulate,
+				introduction: introductionBlockPopulate,
+				blocks: {
+					on: {
+						"block.list": listPopulateBlock,
+						"block.info": infoBlockPopulate,
+						"block.full-section": fullSectionPopulateBlock,
+					},
+				},
+			},
+		});
+
+		return results?.[0] ?? null;
+	},
+}));
+
+const heroPopulate = {
+	fields: ["title", "description", "colored_words", "align_content"],
+	populate: {
+		cover: imagePopulate,
+	},
+};
+
