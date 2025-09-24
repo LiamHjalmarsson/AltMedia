b87e29d8a65cdb3e39d8010bb684b7bf1c23b596
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-09-24 14:12:21 +0200

    blocks

--- client/app/components/blocks/BlocksRenderer.vue
+++ client/app/components/blocks/BlocksRenderer.vue
@@ -7,12 +7,12 @@ defineProps<{ blocks: Blocks[] }>();
 <template>
 	<template v-for="block in blocks" :key="`${block.__component}-${block.id}`">
 		<Hero v-if="block.__component === 'block.hero'" :block="block" />
-		<FeaturedServices v-if="block.__component === 'block.featured-services'" :block="block" />
-		<FeaturedOffers v-if="block.__component === 'block.featured-offers'" :block="block" />
 		<List v-if="block.__component === 'block.list'" :block="block" />
 		<FullSection v-if="block.__component === 'block.full-section'" :block="block" />
-		<FeaturedProjects v-if="block.__component === 'block.featured-projects'" :block="block" />
 		<Faq v-if="block.__component === 'block.faq'" :block="block" />
-		<FeaturedArticles v-else-if="block.__component === 'block.featured-articles'" :block="block" />
+		<FeaturedServices v-if="block.__component === 'block.featured-services'" :block="block" />
+		<FeaturedOffers v-if="block.__component === 'block.featured-offers'" :block="block" />
+		<FeaturedProjects v-if="block.__component === 'block.featured-projects'" :block="block" />
+		<FeaturedArticles v-if="block.__component === 'block.featured-articles'" :block="block" />
 	</template>
 </template>
