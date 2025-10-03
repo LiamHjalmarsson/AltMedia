c25e0f35c78c0dd36ef49077628ccc11e9022942
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2025-10-03 09:31:39 +0200

    List

--- client/app/components/blocks/list/List.vue
+++ client/app/components/blocks/list/List.vue
@@ -17,7 +17,7 @@ defineProps<{ block: List }>();
 					v-for="(item, i) in block.items"
 					:key="item.id"
 					class="grid grid-cols-2 gap-xxl items-center w-full group overflow-hidden">
-					<div :class="(i + 1) % 2 === 0 ? 'md:order-2' : 'md:order-1'">
+					<div :class="(i + 1) % 2 === 0 ? 'md:order-2 pl-xxl' : 'md:order-1 pr-xxl'">
 						<div class="flex items-center">
 							<span
 								class="text-heading-lg sm:text-heading-xl md:text-heading-xxl lg:text-heading-xxxl xl:text-heading-xxxxl font-extrabold text-secondary mr-md lg:mr-lg font-heading">
@@ -32,12 +32,14 @@ defineProps<{ block: List }>();
 						<StrapiBlocksText v-if="item.content?.length" :nodes="item.content || []" class="text-lg" />
 					</div>
 
-					<div :class="(i + 1) % 2 === 0 ? 'md:order-1' : 'md:order-2'" class="overflow-hidden rounded-2xl">
+					<div
+						:class="(i + 1) % 2 === 0 ? 'md:order-1 pr-xxl' : 'md:order-2 pl-xxl'"
+						class="overflow-hidden rounded-2xl">
 						<NuxtImg
 							v-if="item.icon && item.icon.is_image"
 							:src="item.icon.image?.url"
 							:alt="item.icon.image?.alternativeText || ''"
-							class="w-full rounded-xl group-hover:scale-110 transition duration-300 object-cover max-h-[550px]" />
+							class="w-full rounded-xl object-cover max-h-[550px]" />
 					</div>
 				</div>
 			</Grid>
