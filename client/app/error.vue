f410bf662e0ddfd2379393468225cdb05bf2c3f4
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2025-10-04 22:18:17 +0200

    p

--- client/app/error.vue
+++ client/app/error.vue
@@ -10,7 +10,9 @@ defineProps({
 	<div class="h-screen flex justify-center items-center p-xxl">
 		<div class="text-center container">
 			<p class="font-bold text-primary text-[144px]">{{ error?.statusCode }}</p>
+
 			<h1 class="text-heading-4xl font-bold mb-xxl">Detta är inte sidan du letar efter!</h1>
+
 			<NuxtLink to="/" class="px-lg py-sm bg-primary font-bold text-lg text-white rounded-full"
 				>Ta mig tillbaks</NuxtLink
 			>
