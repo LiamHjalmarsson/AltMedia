3687ca389aed8f946c74a00459cedcd4bfeb2fec
Liam Hjalmarsson <liam.hjalmarsson@hotmail.com>
2025-11-14 09:55:04 +0100

    building

--- cms/src/api/build-project-page/controllers/build-project-page.ts
+++ cms/src/api/build-project-page/controllers/build-project-page.ts
@@ -8,38 +8,38 @@ export default factories.createCoreController("api::build-project-page.build-pro
 	async find(ctx) {
 		const entity = await strapi.documents("api::build-project-page.build-project-page").findFirst({
 			fields: ["id", "title", "description", "show_summary_panel", "title"],
-			// populate: {
-			// 	steps: {
-			// 		fields: ["id", "title", "subtitle", "description", "type", "clickable_relations"],
-			// 		populate: {
-			// 			services: {
-			// 				fields: ["id", "title"],
-			// 			},
-			// 			subservices: {
-			// 				fields: ["id", "title"],
-			// 			},
-			// 			questions: {
-			// 				fields: ["id", "help_text", "title", "type"],
-			// 				populate: {
-			// 					options: {
-			// 						fields: ["id", "label", "value", "is_static"],
-			// 					},
-			// 					conditional: {
-			// 						fields: ["id", "trigger_value", "placeholder", "label", "type"],
-			// 					},
-			// 				},
-			// 			},
-			// 		},
-			// 	},
-			// 	seo: {
-			// 		fields: ["meta_title", "meta_description", "meta_canonical_url", "prevent_index"],
-			// 		populate: {
-			// 			meta_image: {
-			// 				fields: ["alternativeText", "width", "url", "provider"],
-			// 			},
-			// 		},
-			// 	},
-			// },
+			populate: {
+				steps: {
+					fields: ["id", "title", "subtitle", "description", "type", "clickable_relations"],
+					populate: {
+						services: {
+							fields: ["id", "title"],
+						},
+						subservices: {
+							fields: ["id", "title"],
+						},
+						questions: {
+							fields: ["id", "help_text", "title", "type"],
+							populate: {
+								options: {
+									fields: ["id", "label", "value", "is_static"],
+								},
+								conditional: {
+									fields: ["id", "trigger_value", "placeholder", "label", "type"],
+								},
+							},
+						},
+					},
+				},
+				seo: {
+					fields: ["meta_title", "meta_description", "meta_canonical_url", "prevent_index"],
+					populate: {
+						meta_image: {
+							fields: ["alternativeText", "width", "url", "provider"],
+						},
+					},
+				},
+			},
 		});
 
 		if (!entity) {
